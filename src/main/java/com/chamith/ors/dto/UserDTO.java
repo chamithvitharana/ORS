bc77a023f2474ca5460d02e5ebe1b333105e1d2f
chamith <chamithvitharana998@gmail.com>
2024-08-29 11:02:48 +0530

    Create user dto class

---
+++ src/main/java/com/chamith/ors/dto/UserDTO.java
@@ -0,0 +1,49 @@
+package com.chamith.ors.dto;
+
+public class UserDTO {
+    private String firstName;
+    private String lastName;
+    private String mobile;
+    private String password;
+    private String userType;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+}
