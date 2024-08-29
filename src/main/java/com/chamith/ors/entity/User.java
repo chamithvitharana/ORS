664ebaac7595158e649574dabe6ed902693f910f
chamith <chamithvitharana998@gmail.com>
2024-08-29 10:56:56 +0530

    Create user related entities

---
+++ src/main/java/com/chamith/ors/entity/User.java
@@ -0,0 +1,75 @@
+package com.chamith.ors.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String mobile;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "userTypeId", nullable = false)
+    private UserType userType;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+}
---
+++ src/main/java/com/chamith/ors/entity/UserType.java
@@ -0,0 +1,5 @@
+package com.chamith.ors.entity;
+
+public enum UserType {
+    ADMIN, CUSTOMER, DELIVERY_COORDINATOR, RECEPTIONIST, RESTAURENT_MANAGER;
+}
