d3cd15720166a2c11f40c65a2e7d2e5622227f34
chamith <chamithvitharana998@gmail.com>
2024-08-29 10:50:00 +0530

    Initial Commit

---
+++ src/main/java/com/chamith/ors/OrsApplication.java
@@ -0,0 +1,13 @@
+package com.chamith.ors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chamith/ors/OrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chamith.ors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
