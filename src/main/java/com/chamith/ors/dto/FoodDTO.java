bacbd4c44448b6ed609b150466ddbe2e412b65e6
chamith <chamithvitharana998@gmail.com>
2024-08-31 13:55:15 +0530

    Add food dto class

---
+++ src/main/java/com/chamith/ors/dto/FoodDTO.java
@@ -0,0 +1,49 @@
+package com.chamith.ors.dto;
+
+public class FoodDTO {
+    private int id;
+    private String name;
+    private double price;
+    private String imagePath;
+    private int availability;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public int getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(int availability) {
+        this.availability = availability;
+    }
+}
