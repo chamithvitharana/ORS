0d3c513b48f34519ad47894fd2beaed00cf2d89f
chamith <chamithvitharana998@gmail.com>
2024-09-02 12:22:50 +0530

    Add order item entity

---
+++ src/main/java/com/chamith/ors/entity/OrderItem.java
@@ -0,0 +1,50 @@
+package com.chamith.ors.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "orderId", nullable = false)
+    private Order order;
+    @ManyToOne
+    @JoinColumn(name = "foodItemId", nullable = false)
+    private FoodItem foodItem;
+    @Column(nullable = false)
+    private int quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public FoodItem getFoodItem() {
+        return foodItem;
+    }
+
+    public void setFoodItem(FoodItem foodItem) {
+        this.foodItem = foodItem;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
