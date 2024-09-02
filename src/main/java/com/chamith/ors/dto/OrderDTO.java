67c95c2a4606bf7d566326ba958030c5120b1ff8
chamith <chamithvitharana998@gmail.com>
2024-09-02 11:56:56 +0530

    Add order dto class

---
+++ src/main/java/com/chamith/ors/dto/OrderDTO.java
@@ -0,0 +1,63 @@
+package com.chamith.ors.dto;
+
+import java.util.List;
+
+public class OrderDTO {
+    private int customerId;
+    private String orderType;
+    private String paymentMethod;
+    private List<FoodItemDTO> foodItems;
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(String orderType) {
+        this.orderType = orderType;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public List<FoodItemDTO> getFoodItems() {
+        return foodItems;
+    }
+
+    public void setFoodItems(List<FoodItemDTO> foodItems) {
+        this.foodItems = foodItems;
+    }
+
+    public static class FoodItemDTO {
+        private int foodItemId;
+        private int quantity;
+
+        public int getFoodItemId() {
+            return foodItemId;
+        }
+
+        public void setFoodItemId(int foodItemId) {
+            this.foodItemId = foodItemId;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+    }
+}
