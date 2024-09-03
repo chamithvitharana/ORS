2943001c90c3e767dc2674a834770fceea105470
chamith <chamithvitharana998@gmail.com>
2024-09-03 11:39:23 +0530

    Add new end point to update the status of order

--- src/main/java/com/chamith/ors/controller/OrderController.java
+++ src/main/java/com/chamith/ors/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.chamith.ors.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +32,19 @@ public class OrderController {
             return ResponseEntity.ok("Successfully added the order to queue!");
         } else {
             return ResponseEntity.badRequest().body("Something went wrong! Please try again later.");
+        }
+    }
+
+    @GetMapping("/update")
+    public ResponseEntity<String> updateStatus(@RequestParam("orderId") long orderId,
+                                               @RequestParam("status") String newStatus) {
+
+        boolean isUpdated = orderService.updateStatus(orderId, newStatus);
+
+        if(isUpdated) {
+            return ResponseEntity.ok("Status updated successfully!");
+        } else {
+            return ResponseEntity.badRequest().body("Status not updated! Please try again.");
         }
     }
 }
