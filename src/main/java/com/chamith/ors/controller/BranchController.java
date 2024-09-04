57ff29fc3c2268735fe859f765222fb4d443e468
chamith <chamithvitharana998@gmail.com>
2024-09-04 10:55:25 +0530

    Create branch controller classes

---
+++ src/main/java/com/chamith/ors/controller/BranchController.java
@@ -0,0 +1,32 @@
+package com.chamith.ors.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.chamith.ors.service.BranchService;
+
+@RestController
+@RequestMapping("/branch")
+public class BranchController {
+    @Autowired
+    private final BranchService branchService;
+
+    public BranchController(BranchService branchService) {
+        this.branchService = branchService;
+    }
+
+    @GetMapping("/add")
+    public ResponseEntity<String> addNewBranch(@RequestParam("branch") String name) {
+        boolean isSuccess = branchService.addBranch(name);
+
+        if(isSuccess) {
+            return ResponseEntity.ok("Branch added successfully!");
+        } else {
+            return ResponseEntity.badRequest().body("This branch is already added!");
+        }
+    }
+}
