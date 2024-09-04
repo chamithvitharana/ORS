8fe0ceb6434dcd6927859d027332def2344b740f
chamith <chamithvitharana998@gmail.com>
2024-09-04 10:09:32 +0530

    Add new method to find category

--- src/main/java/com/chamith/ors/repo/FoodCategoryRepository.java
+++ src/main/java/com/chamith/ors/repo/FoodCategoryRepository.java
@@ -1,8 +1,11 @@
 package com.chamith.ors.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.chamith.ors.entity.FoodCategory;
 
 public interface FoodCategoryRepository extends JpaRepository<FoodCategory, Long> {
+    Optional<FoodCategory> findByName(String name);
 }
