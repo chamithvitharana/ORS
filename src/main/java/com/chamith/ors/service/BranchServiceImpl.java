bad6a9fc3c136dbc669f5f0d671c4038729dd610
chamith <chamithvitharana998@gmail.com>
2024-09-05 18:35:21 +0530

    Add @Service annotation

--- src/main/java/com/chamith/ors/service/BranchServiceImpl.java
+++ src/main/java/com/chamith/ors/service/BranchServiceImpl.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.chamith.ors.entity.Branch;
 import com.chamith.ors.repo.BranchRepository;
 
+@Service
 public class BranchServiceImpl implements BranchService {
     @Autowired
     private final BranchRepository branchRepository;
