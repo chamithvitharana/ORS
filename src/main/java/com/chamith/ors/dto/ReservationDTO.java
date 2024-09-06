e8d0c2d4cdef8c3005cd4ce3b429be954efd938e
chamith <chamithvitharana998@gmail.com>
2024-09-06 17:24:37 +0530

    Add reservation dto class

---
+++ src/main/java/com/chamith/ors/dto/ReservationDTO.java
@@ -0,0 +1,76 @@
+package com.chamith.ors.dto;
+
+public class ReservationDTO {
+    private long id;
+    private String datetime;
+    private int tableNo;
+    private int seatCount;
+    private String mobile;
+    private String customerName;
+    private int branchId;
+    private String branchName;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
+
+    public int getTableNo() {
+        return tableNo;
+    }
+
+    public void setTableNo(int tableNo) {
+        this.tableNo = tableNo;
+    }
+
+    public int getSeatCount() {
+        return seatCount;
+    }
+
+    public void setSeatCount(int seatCount) {
+        this.seatCount = seatCount;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public int getBranchId() {
+        return branchId;
+    }
+
+    public void setBranchId(int branchId) {
+        this.branchId = branchId;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+}
